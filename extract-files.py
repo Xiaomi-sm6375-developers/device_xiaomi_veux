41682517647589c837aec5b947f879d7910ec79d
Onelots <onelots@onelots.fr>
2025-11-14 10:00:03 +0100

    veux: Kang proprietary files from stone
    Signed-off-by: Onelots <onelots@onelots.fr>

--- extract-files.py
+++ extract-files.py
@@ -8,7 +8,10 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
-
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -19,15 +22,27 @@ namespace_imports = [
     'hardware/qcom-caf/sm8350',
     'hardware/qcom-caf/wlan',
     'hardware/xiaomi',
-    'vendor/qcom/opensource/commonsys/display',
     'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
     'vendor/qcom/opensource/dataservices',
     'vendor/qcom/opensource/display',
 ]
 
-
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'vendor.qti.diaghal@1.0',
+        'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.ims.callcapability@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+}
 
 blob_fixups: blob_fixups_user_type = {
     'odm/etc/build_*.prop': blob_fixup()
@@ -38,7 +53,7 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'^(?!service)(.*)wfdservice$', r'\g<1>wfdservice64'),
     
     'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .remove_needed('android.hidl.base@1.0.so'),
+        .replace_needed('android.hidl.base@1.0.so', 'libinput_shim.so'),
     
     'vendor/etc/camera/camxoverridesettings.txt': blob_fixup()
         .regex_replace(r'0x10080', r'0')
@@ -47,20 +62,19 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/init/init.batterysecret.rc': blob_fixup()
         .regex_replace(r'on charger', r'on property:init.svc.vendor.charger=running'),
     
-    'vendor/etc/libnfc-pn557.conf': blob_fixup()
-        .call(lambda file_path, content: 
-              content if "NXP RF" in content 
-              else content + '\n' + open(f"{SRC}/vendor/libnfc-nxp_RF.conf").read()),
-    
     'vendor/lib64/android.hardware.secure_element@1.0-impl.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
-    
-    'vendor/lib64/camera/components/com.qti.node.mialgocontrol.so': blob_fixup()
-        .add_needed('libpiex_shim.so'),
-    
-    ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
-        .add_needed('libcrypto_shim.so'),
-    
+
+    ('vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+
+    'vendor/lib64/libgoodixhwfingerprint.so': blob_fixup()
+        .replace_needed('libvendor.goodix.hardware.biometrics.fingerprint@2.1.so', 'vendor.goodix.hardware.biometrics.fingerprint@2.1.so'),
+
     ('vendor/lib64/libdlbdsservice.so', 
      'vendor/lib/libstagefright_soft_ac4dec.so', 
      'vendor/lib/libstagefright_soft_ddpdec.so'): blob_fixup()
@@ -72,6 +86,15 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'(DEFAULT_OFFHOST_ROUTE.*?)0x01', r'\g<1>0xC0')
         .regex_replace(r'(OFFHOST_ROUTE_ESE.*?)01', r'\g<1>C0')
         .regex_replace(r'$', r'\nDEFAULT_NFCF_ROUTE=0xC0'),
+
+    ('vendor/lib64/libwvhidl.so','vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+
+    ('system_ext/lib/libwfdmmsrc_system.so','system_ext/lib64/libwfdmmsrc_system.so'): blob_fixup()
+        .add_needed('libgui_shim.so'),
+    
+    ('system_ext/lib/libwfdservice.so','system_ext/lib64/libwfdservice.so'): blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
 }
 
 
@@ -79,6 +102,7 @@ module = ExtractUtilsModule(
     'veux',
     'xiaomi',
     blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
 
