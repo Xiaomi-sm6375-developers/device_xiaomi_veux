ea14021e1f3d96dfafe9a4b3c49b978870726109
Onelots <onelots@onelots.fr>
2025-11-16 00:39:24 +0100

    veux: do not build com.dsi.ant
    Signed-off-by: Onelots <onelots@onelots.fr>

--- device.mk
+++ device.mk
@@ -95,7 +95,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
-    com.dsi.ant@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
